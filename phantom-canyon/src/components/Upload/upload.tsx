3ac390ac51206a687d482f448baeb85b8af67d18
zhangmingle <15szhang2@gmail.com>
2021-05-01 14:38:02 +0800

    feat: upload 组件

--- phantom-canyon/src/components/Upload/upload.tsx
+++ phantom-canyon/src/components/Upload/upload.tsx
@@ -1,24 +1,54 @@
-import React, {ChangeEvent, FC, useRef} from 'react'
+import React, {ChangeEvent, FC, useRef, useState} from 'react'
 import axios from 'axios'
 
 import Button, {ButtonType} from '../Button/button'
 
+export type UploadFileStatus = 'ready' | 'uploading' | 'success' | 'error'
+export interface UploadFile {
+    uid: string;
+    size: number;
+    name: string;
+    status?: UploadFileStatus;
+    percent?: number;
+    raw?: File;
+    response?: any;
+    error?: any;
+  }
 export interface UploadProps {
     action: string;
+    beforeUpload?: (file: File) => boolean | Promise<File>
     onProgress?: (percentage: number, file: File) => void;
     onSuccess?: (data: any, file: File) => void;
     onError?: (err: any, file: File) => void;
+    onChange?: (file: File) => void;
 }
 
 export const Upload: FC<UploadProps> = (props) => {
     const {
         action,
+        beforeUpload,
         onProgress,
         onSuccess,
-        onError
+        onError,
+        onChange
     } = props
 
     const fileInut = useRef<HTMLInputElement>(null)
+
+    const [fileList, setFileList] = useState<UploadFile[]>([])
+
+    const updateFileList = (updateFile: UploadFile, updateObj: Partial<UploadFile>) => {
+        setFileList(prevList => {
+            return prevList.map(file => {
+                if(file.uid === updateFile.uid) {
+                    return {...file, ...updateObj}
+                } else {
+                    return file
+                }
+            })
+        })
+    }
+
     const handleClick = () => {
         if(fileInut.current) {
             fileInut.current.click()
@@ -39,29 +69,65 @@ export const Upload: FC<UploadProps> = (props) => {
         let postFiles = Array.from(files) 
 
         postFiles.forEach(file => {
-            const formData = new FormData()
-            formData.append(file.name, file)
-            axios.post(action, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                },
-                onUploadProgress: (e) => {
-                    let percentage = Math.round((e.loaded * 100) / e.total) || 0;
-                    if(percentage < 100) {
-                        if(onProgress) {
-                            onProgress(percentage, file)
-                        }
+            if(!beforeUpload) {
+                post(file)
+            } else {
+                const result = beforeUpload(file)
+                if (result && result instanceof Promise) {
+                    result.then(processedFile => {
+                        post(processedFile)
+                    }) 
+                } else if(result !== false) {
+                    post(file)
+                }
+            }
+        })
+    }
+
+    const post = (file: File) => {
+        let _file: UploadFile = {
+            uid: Date.now() + 'upload-file',
+            status: 'ready',
+            name: file.name,
+            size: file.size,
+            percent: 0,
+            raw: file
+        }
+        setFileList([_file, ...fileList])
+
+        const formData = new FormData()
+        formData.append(file.name, file)
+        axios.post(action, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            },
+            onUploadProgress: (e) => {
+                let percentage = Math.round((e.loaded * 100) / e.total) || 0;
+                if(percentage < 100) {
+                    updateFileList(_file, {percent: percentage, status: 'uploading'})
+                    if(onProgress) {
+                        onProgress(percentage, file)
                     }
                 }
-            }).then(resp => {
-                console.log(resp)
-                if(onSuccess) {
-                    onSuccess(resp.data, file)
-                }
-            }).catch(err => {
-                if(onError)
-                    onError(err, file)
-            })
+            }
+        }).then(resp => {
+            updateFileList(_file, {status: 'success', response: resp.data})
+            if(onSuccess) {
+                onSuccess(resp.data, file)
+            }
+            if(onChange) {
+                onChange(file)
+            }
+        }).catch(err => {
+            updateFileList(_file, {status: 'error', error: err})
+            if(onError) {
+                onError(err, file)
+            }
+            if(onChange) {
+                onChange(file)
+            }
+                
+            
         })
     }
 
