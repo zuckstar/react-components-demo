98132bc54433d0f1ebdec130234d96043696a590
ZuckStar <527489516@qq.com>
2021-05-27 22:22:41 +0800

    feat: 引用测试

--- my-react-ts-app/src/views/Hello.tsx
+++ my-react-ts-app/src/views/Hello.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import HelloCom from '../components/Hello'
-import { Button } from 'phantom-canyon'
+import { Button, Icon, Menu} from 'phantom-canyon'
 
 const Hello: React.FC = () => {
 
@@ -8,6 +8,18 @@ const Hello: React.FC = () => {
     <div className="App">
       <HelloCom message="Hello World!" />
       <Button>你好</Button>
+      <Icon icon="check-circle" theme="primary" size="8x" />
+      <Menu defaultIndex='0' >
+        <Menu.Item>
+          cool link
+        </Menu.Item>
+        <Menu.Item disabled>
+          disabled
+        </Menu.Item> 
+        <Menu.Item>
+          cool link 2
+        </Menu.Item> 
+      </Menu>
     </div>
   );
 }
